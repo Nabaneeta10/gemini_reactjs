a5d729bc68ae8f111f658d6f6a829b2aaa0ce88a
Nabaneeta10 <nabaneetamohanta10@gmail.com>
2024-07-24 22:43:29 +0530

    changes

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from '../src/components/Sidebar/Sidebar'
+import Main from '../src/components/Main/Main'
+
+
+const App = () => {
+  return (
+    <>
+        <Sidebar />
+        <Main />
+    </>
+    
+  )
+}
+
+export default App
